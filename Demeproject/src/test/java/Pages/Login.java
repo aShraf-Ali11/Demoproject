1a8a87c471c00ea752f1da3afaeb4951465d57da
Net <Net@NET000L085>
2020-10-13 10:23:32 +0530

    this is my first commit

---
+++ Demeproject/src/test/java/Pages/Login.java
@@ -0,0 +1,48 @@
+package Pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Login 
+{
+	@Test
+	public static void Browser() {
+
+        // declaration and instantiation of objects/variables
+    	//System.setProperty("webdriver.gecko.driver","C:\\geckodriver.exe");
+		WebDriverManager.chromedriver().setup();
+    	WebDriver driver = new ChromeDriver();
+		//comment the above 2 lines and uncomment below 2 lines to use Chrome
+		//System.setProperty("webdriver.chrome.driver","G:\\chromedriver.exe");
+		//WebDriver driver = new ChromeDriver();
+    	
+        String baseUrl = "http://demo.guru99.com/test/newtours/";
+        String expectedTitle = "Welcome: Mercury Tours";
+        String actualTitle = "";
+        System.out.println("Test12 Passed!");
+
+        // launch Fire fox and direct it to the Base URL
+        driver.get(baseUrl);
+
+        // get the actual value of the title
+        actualTitle = driver.getTitle();
+
+        /*
+         * compare the actual title of the page with the expected one and print
+         * the result as "Passed" or "Failed"
+         */
+        if (actualTitle.contentEquals(expectedTitle)){
+            System.out.println("Test Passed!");
+        } else {
+            System.out.println("Test Failed");
+        }
+       
+        //close Fire fox
+      //  driver.close();
+       
+    }
+
+}
